e2f0c84fafbb60b9a0b6a5bebc4d230f24242543
claire.chen <claire.chen@ww.com>
2023-04-30 08:51:57 -0400

    51. create 2 new components to add new expenses

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,26 @@
+import React, { useState } from 'react';
+
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 //function ExpenseItem(props) {
 const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(props.title);
+
+    // let title = props.title;
 
     const clickHandler = () => {
-        console.log('Clicked!!');
+        setTitle('Updated!');
+        // title = 'Updated!'
+        console.log(title);
     };
 
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
             <button onClick={clickHandler}>Change Title</button>
