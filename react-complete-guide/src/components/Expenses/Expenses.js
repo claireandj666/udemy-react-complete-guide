dd8d3bf643438fc9246d7f3d7ec878efa9b8db00
claire.chen <claire.chen@ww.com>
2023-05-14 15:31:54 -0400

    64. dynamically enter/output data

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -15,7 +15,10 @@ const Expenses = (props) => {
     return (
         <div>
             <Card className="expenses">
-                <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
+                <ExpensesFilter 
+                    selected={filteredYear} 
+                    onChangeFilter={filterChangeHandler}
+                />
                 {props.items.map((expense) => (
                     <ExpenseItem 
                         title={expense.title} 
