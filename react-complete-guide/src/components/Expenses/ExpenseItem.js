6363ed1d266016804b5cdbb9f1f5e8c6fc10ee6a
claire.chen <claire.chen@ww.com>
2023-05-24 09:45:31 -0700

    67. add and restructure a few components to display the 'No expenses found' message

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -17,13 +17,15 @@ const ExpenseItem = (props) => {
     };
 
     return (
-        <Card className="expense-item">
+        <li>
+            <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
                 <h2>{title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
         </Card>
+        </li>
     );
 }
 
--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
+
 import './Expenses.css'
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 
 //function Expenses (props) {
 const Expenses = (props) => {
@@ -17,18 +18,6 @@ const Expenses = (props) => {
         return expense.date.getFullYear().toString() === filteredYear;
     });
 
-    let expenseContent = <p>No expenses found.</p>;
-    if (filteredExpenses.length > 0) {
-        expenseContent = filteredExpenses.map((expense) => (
-            <ExpenseItem 
-                key={expense.id}
-                title={expense.title} 
-                amount={expense.amount} 
-                date={expense.date} 
-            />
-        ));
-    }
-
     return (
         <div>
             <Card className="expenses">
@@ -36,7 +25,7 @@ const Expenses = (props) => {
                     selected={filteredYear} 
                     onChangeFilter={filterChangeHandler}
                 />
-                {expenseContent}   
+                <ExpensesList items={filteredExpenses} />  
             </Card>
         </div>
     )
