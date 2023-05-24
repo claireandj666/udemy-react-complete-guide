0407daab4832c72a960b7fd1222e04607128e7c9
claire.chen <claire.chen@ww.com>
2023-05-24 11:59:02 -0700

    Assignment 4: add two new button, conditional rendering

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate';
@@ -6,22 +6,13 @@ import Card from '../UI/Card';
 
 //function ExpenseItem(props) {
 const ExpenseItem = (props) => {
-    const [title, setTitle] = useState(props.title);
-
-    // let title = props.title;
-
-    const clickHandler = () => {
-        setTitle('Updated!');
-        // title = 'Updated!'
-        console.log(title);
-    };
 
     return (
         <li>
             <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
         </Card>
