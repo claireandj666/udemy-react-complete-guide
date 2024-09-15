faa8b72c05f815b94625da3d720131fe640b8ae3
claireandj666 <claire.yandi.chen@gmail.com>
2024-09-15 12:48:18 -0400

    updated code

--- react-complete-guide/src/components/NewExpense/ExpenseForm.js
+++ react-complete-guide/src/components/NewExpense/ExpenseForm.js
@@ -75,3 +75,103 @@ const ExpenseForm = (props) => {
 };
 
 export default ExpenseForm;
+
+const useStyles = makeStyles({
+    gridRow: {
+        flexGrow: 1,
+        color: '#483D8B'
+    }
+})
+
+export default function Home() {
+    const cardsRow1: ApplicationCardData[] = [
+        {
+            id: 0,
+            header: 'AAA',
+            buttons: [
+                {
+                    linkDestionation: "",
+                    isDisabled: false,
+                }
+            ]
+        },
+        {
+            id: 1,
+            header: 'BBB',
+            buttons: [
+                {
+                    linkDestionation: "",
+                    isDisabled: false,
+                }
+            ]
+        },
+        {
+            id: 2,
+            header: 'CCC',
+            buttons: [
+                {
+                    linkDestionation: "",
+                    isDisabled: false,
+                }
+            ]
+        },
+    ];
+
+    const cardsRow2: ApplicationCardData[] = [
+        {
+            id: 3,
+            header: 'DDD',
+            buttons: [
+                {
+                    linkDestionation: "",
+                    isDisabled: false,
+                }
+            ]
+        },
+        {
+            id: 4,
+            header: 'Environment Setup',
+            buttons: [
+                {
+                    linkDestionation: "",
+                    isDisabled: false,
+                }
+            ]
+        },
+        {
+            id: 5,
+            header: 'FFF',
+            buttons: [
+                {
+                    linkDestionation: "",
+                    isDisabled: false,
+                }
+            ]
+        },
+    ];
+
+    const rowsData = [cardsRow1, cardsRow2]
+
+    const rows = rowsData.map((row, index) => {
+        return (
+            <div key={`row-${index}`} className={classes.gridRow}>
+                <h3>{rowHeaders[index]}</h3>
+                <Grid item xs={7}>
+                    <Grid container spacing={2}>
+                        {row.map((cardDatum) => (
+                            <Grid item xs={cardWidth} key={cardDatum.id}>
+
+                            </Grid>
+                        ))}
+                    </Grid>
+                </Grid>
+            </div>
+        )
+    })
+
+    return (
+        <Grid container className={classes.gridRow} spacing={2}>
+            {rows}
+        </Grid>
+    )
+}
